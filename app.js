d027f0a99c7769e0193b2f425ce43fceb88beeca
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-05-03 20:25:54 +0100

    Add database configuration, middleware and controllers

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-import express  from 'express';
-import {PORT} from './config/env.js'
-import userRouter from './routes/user.routes.js';
-import authRouter from './routes/auth.routes.js';
-import subscriptionRouter from './routes/subscription.routes.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
+// Parses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json());
+
+// Parses URL-encoded data (e.g., from HTML forms) and puts it in req.body
+app.use(express.urlencoded({ extended: false }));
+
+// Parses cookies from the HTTP request and adds them to req.cookies
+app.use(cookieParser());
 // Initialize an app
 const app = express();
 // Using use when we want to say which routes we want to use
 // api/v1/auth/sign-up
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
-// Create a route, wee need to add parameter with route and req, res
-app.get('/', (req, res) => {
-    res.send('Welcome')
+app.use(errorMiddleware);
+
+// Create a route, we need to add parameter with route and req, res
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 // Listen to specific port
-app.listen(PORT,  () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-})
+app.listen(PORT, async () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+
+  await connectToDatabase();
+});
 
 export default app;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,11 @@
 import { Router } from "express";
+import { signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 // Create a path to auth page
-authRouter.post('/sign-up', (req, res) => {
-    res.send({title: 'Sign Up'})
-})
-authRouter.post('/sign-in', (req, res) => {
-    res.send({title: 'Sign In'})
-})
-authRouter.post('/sign-out', (req, res) => {
-    res.send({title: 'Sign Out'})
-})
+authRouter.post('/sign-up', signUp)
+// authRouter.post('/sign-in', signIn)
+// authRouter.post('/sign-out', signOut)
 
 export default authRouter;
